f2e41eca18f6a35ae867ba9b6cc693d84800d614
linus muema <linus.m.muema@gmail.com>
2021-02-10 16:59:27 +0300

    - register the routes

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
+// Import the routes modules
+const authRoute = require('./routes/auth');
+const usersRoute = require('./routes/users');
+const blogsRoute = require('./routes/blogs');
+
+// Register the routes
+app.use('/api/auth', authRoute);
+app.use('/api/users', usersRoute);
+app.use('/api/blogs', blogsRoute);
+
+// Start the server
 app.listen(process.env.PORT, async () => {
     console.log('server started : 2400')
 
@@ -18,3 +29,5 @@ app.listen(process.env.PORT, async () => {
     await mongoose.connect(process.env.MONGODB_URL, options)
     console.log('connected to DB successfully')
 });
+
+module.exports = app;
